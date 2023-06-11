3796cdbb7e3b87bd861fb744bb724ce73cbc8e13
Melike Yetişer <126954765+melikeyetiser@users.noreply.github.com>
2023-06-11 11:00:00 +0300

    Add files via upload

---
+++ SQL/sql_odev4.sql
@@ -0,0 +1,12 @@
+select distinct replacement_cost from film;
+
+select count(distinct replacement_cost) from film;
+
+select count(*) from film
+where title like 'T%' and rating = 'G';
+
+select count(*) from country
+where country like '_____'; -- countries that consist of five characters
+
+select count(*) from city
+where city ilike '%r';
