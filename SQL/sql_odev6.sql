c4ba762688e5cbdbae75656d0f3ca02f88a273a4
Melike Yetişer <126954765+melikeyetiser@users.noreply.github.com>
2023-06-12 13:27:33 +0300

    Add files via upload

---
+++ SQL/sql_odev6.sql
@@ -0,0 +1,10 @@
+select avg(rental_rate) from film;
+
+select count(*) from film
+where title like 'C%';
+
+select max(length) from film
+where rental_rate = 0.99;
+
+select count(distinct replacement_cost) from film
+where length > 150;
