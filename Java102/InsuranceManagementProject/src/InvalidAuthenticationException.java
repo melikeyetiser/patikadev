012a1a4231738ae2022a9963c18b31e1821a74aa
Melike Yetişer <126954765+melikeyetiser@users.noreply.github.com>
2023-08-24 21:13:41 +0300

    add insurance management system project

---
+++ Java102/InsuranceManagementProject/src/InvalidAuthenticationException.java
@@ -0,0 +1,7 @@
+public class InvalidAuthenticationException extends Throwable {
+
+    public InvalidAuthenticationException(String msg){
+        super(msg);
+        System.out.println("Invalid password !");
+    }
+}
